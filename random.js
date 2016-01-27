3e9cf03b43e94deba242c92d88cbae588aa137f6
Vallealto Webtech <agm@vallealto.es>
2016-01-27 10:19:59 +0100

    Random versión 2
    Mejoras al código del random

--- random.js
+++ random.js
@@ -1,8 +1,10 @@
 // Math.random devuelve un numero aleatorio entre 0 y 1
 var numero = Math.random();
 
+var str = ' MAYOR que 0,5';
+
 if(numero <= 0.5){
-	console.log('\n' + numero + ' MENOR que 0,5 \n');
-}else{
-	console.log('\n' + numero + ' MAYOR que 0,5 \n');
+	str = 'MENOR que 0,5';
 }
+
+console.log('\n' + numero + str +' \n');
